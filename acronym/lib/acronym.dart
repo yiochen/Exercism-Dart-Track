535b89f132da721e60e68290c28898c31ed3136b
Yiou Chen <chen.yiou1992@gmail.com>
2019-05-25 21:40:42 -0700

    more questions

--- acronym/lib/acronym.dart
+++ acronym/lib/acronym.dart
@@ -1,3 +1,7 @@
 class Acronym {
-  // Put your code here
+  String abbreviate(String words) {
+    final splitted = words.split(new RegExp(r"[ \-_]+"));
+    print(splitted);
+    return splitted.map((elem) => elem[0].toUpperCase()).join('');
+  }
 }
--- bob/lib/bob.dart
+++ bob/lib/bob.dart
@@ -1,3 +1,37 @@
+bool isShouting(String words) {
+  final alphabetRegex = new RegExp(r"[a-zA-Z]");
+  final String onlyWords =
+      words.split('').where((part) => alphabetRegex.hasMatch(part)).join('');
+  return onlyWords == onlyWords.toUpperCase() && onlyWords.length > 0;
+}
+
+bool isQuestion(String words) {
+  return words.endsWith("?");
+}
+
+bool isYelledQuestion(String words) {
+  return isQuestion(words) && isShouting(words);
+}
+
+String preprocess(String words) {
+  return words.trim();
+}
+
 class Bob {
-  // Put your code here
+  String response(String words) {
+    final processed = preprocess(words);
+    if (processed == '') {
+      return "Fine. Be that way!";
+    }
+    if (isYelledQuestion(processed)) {
+      return "Calm down, I know what I'm doing!";
+    }
+    if (isQuestion(processed)) {
+      return "Sure.";
+    }
+    if (isShouting(processed)) {
+      return "Whoa, chill out!";
+    }
+    return "Whatever.";
+  }
 }
